bbd106fa73517c3f312e6680a9c9087d82929f30
Ch110pB <chkb2357@gmail.com>
2016-12-29 16:55:42 +0530

    sixth commit

--- harrishog.py
+++ harrishog.py
@@ -17,12 +17,12 @@ cellsize = 8
 ncells = 1
 kvalue = 500 #k for k-means
 
-def calcHog(im,points,patchsize,ncells):
+def calcHog(im,points,patchsize,ncells,idx):
     imsz = im.shape
     halfsize = (np.float32(patchsize) - np.float32(patchsize%2))/2
     valid_points = []
     
-    npoints = len(harrispoints[0])
+    npoints = len(points[idx][0])
     roi = [1,1,patchsize,patchsize]
     
     validPointIdx = np.zeros((1,npoints),np.uint32)
@@ -32,7 +32,7 @@ def calcHog(im,points,patchsize,ncells):
     hogvalues = np.zeros(hogdim,np.float32)
     
     for j in range(npoints):
-        roi[0:2] = [np.uint32(harrispoints[0][j]-halfsize),np.uint32(harrispoints[1][j]-halfsize)]
+        roi[0:2] = [np.uint32(points[idx][0][j]-halfsize),np.uint32(points[idx][1][j]-halfsize)]
         if all(roi[0:2]) >= 1 and roi[1]+roi[3]-1 <= imsz[0] and roi[0]+roi[2]-1 <= imsz[1]:
             im_tmp = im[(roi[0]-1):(roi[0]+roi[2]),(roi[1]-1):(roi[1]+roi[3])]
             hogi = hog.hog(im_tmp,cellsize,'DalalTriggs')
@@ -42,8 +42,8 @@ def calcHog(im,points,patchsize,ncells):
             
     hogvalues = hogvalues[0:validPointCount,:]
     validPointIdx = validPointIdx[0:validPointCount]
-    for m in range(np.size(points[0][0])):
-        valid_points.append([points[0][0][m],points[0][1][m]])
+    for m in range(np.size(points[idx][0])):
+        valid_points.append([points[idx][0][m],points[idx][1][m]])
     return([(hogvalues,valid_points)])
     
     
@@ -64,7 +64,7 @@ for i in range(1,num_org+1):
     harrispoints = np.where(dmax == True)
     points_org.append(harrispoints)
     
-    [(hogvalues,valid_points)] = calcHog(gray,points_org,patchsize,ncells)
+    [(hogvalues,valid_points)] = calcHog(gray,points_org,patchsize,ncells,i-1)
     for m in range(hogvalues.shape[0]):
         hogvalues[m] /= sum(hogvalues[m])
     hogmatrix.append(hogvalues)
@@ -119,7 +119,7 @@ for i in range(1,num_forg+1):
     harrispoints = np.where(dmax == True)
     points_forg.append(harrispoints)
     
-    [(hogvalues,valid_points)] = calcHog(gray,points_forg,patchsize,ncells)
+    [(hogvalues,valid_points)] = calcHog(gray,points_forg,patchsize,ncells,i-1)
     for m in range(hogvalues.shape[0]):
         hogvalues[m] /= sum(hogvalues[m])
     hogmatrix.append(hogvalues)
@@ -188,9 +188,9 @@ for i in range(num_org):
         tri_org_center_temp_1.append((sum(tri_org[i][k])/3)[0])
         tri_org_center_temp_2.append((sum(tri_org[i][k])/3)[1])
     tri_org_center.append([np.array(tri_org_center_temp_1),np.array(tri_org_center_temp_2)])
-    [(hogvalues_tri_org,p)] = calcHog(gray,tri_org_center,patchsize,ncells)
-    for m in range(hogvalues_tri_org.shape[0]):
-        hogvalues_tri_org[m] /= sum(hogvalues_tri_org[m])
+    [(hogvalues_tri_org,p)] = calcHog(gray,tri_org_center,patchsize,ncells,i)
+    for n in range(hogvalues_tri_org.shape[0]):
+        hogvalues_tri_org[n] /= sum(hogvalues_tri_org[n])
     hogmatrix_tri_org.append(hogvalues_tri_org)
     
 hogmatrixcat_tri_org = np.concatenate((hogmatrix_tri_org[:]),axis=0)
@@ -215,7 +215,7 @@ for i in range(num_forg):
         tri_forg_center_temp_1.append((sum(tri_forg[i][k])/3)[0])
         tri_forg_center_temp_2.append((sum(tri_forg[i][k])/3)[1])
     tri_forg_center.append([np.array(tri_forg_center_temp_1),np.array(tri_forg_center_temp_2)])
-    [(hogvalues_tri_forg,p)] = calcHog(gray,tri_forg_center,patchsize,ncells)
+    [(hogvalues_tri_forg,p)] = calcHog(gray,tri_forg_center,patchsize,ncells,i)
     for m in range(hogvalues_tri_forg.shape[0]):
         hogvalues_tri_forg[m] /= sum(hogvalues_tri_forg[m])
     hogmatrix_tri_forg.append(hogvalues_tri_forg)
@@ -223,6 +223,60 @@ for i in range(num_forg):
 hogmatrixcat_tri_forg = np.concatenate((hogmatrix_tri_forg[:]),axis=0)
 kmeancenters_tri_forg = kmeans.kmeans(hogmatrixcat_tri_forg,kvalue,initialization='PLUSPLUS')
 kmeanclusters_tri_forg = kmeans.kmeans_quantize(hogmatrixcat_tri_forg,kmeancenters_tri_forg)
+    
+label_tri_org = []
+clustercount_tri = 0
+
+for i in range(len(hogmatrix_tri_org)):
+    count = 0
+    label2 = []
+    while count < hogmatrix_tri_org[i].shape[0]:
+        label2.append(kmeanclusters_tri_org[clustercount_tri])
+        clustercount_tri += 1
+        count += 1
+    label_tri_org.append(label2)
+
+hist_tri_org=np.zeros((len(label_tri_org),kvalue))
+    
+for i in range(len(label_tri_org)):
+    for j in range(len(label_tri_org[i])):
+        hist_tri_org[i][label_tri_org[i][j]] += 1
+    hist_tri_org[i] /= sum(hist_tri_org[i])
+
+label_tri_forg = []
+clustercount_tri = 0
+
+for i in range(len(hogmatrix_tri_forg)):
+    count = 0
+    label2 = []
+    while count < hogmatrix_tri_forg[i].shape[0]:
+        label2.append(kmeanclusters_tri_forg[clustercount_tri])
+        clustercount_tri += 1
+        count += 1
+    label_tri_forg.append(label2)
+
+hist_tri_forg=np.zeros((len(label_tri_forg),kvalue))
+    
+for i in range(len(label_tri_forg)):
+    for j in range(len(label_tri_forg[i])):
+        hist_tri_forg[i][label_tri_forg[i][j]] += 1
+    hist_tri_forg[i] /= sum(hist_tri_forg[i])
+
+globaldesc_tri_temp = []
+globaldesc_tri = []
+count_tri = 0
+for i in range(num_org):
+    globaldesc_tri_org= []
+    globaldesc_tri_forg = []
+    for j in range(num_org):
+        globaldesc_tri_org.append((abs(hist_tri_org[i]-hist_tri_org[j]),1))
+    globaldesc_tri_temp.append(globaldesc_tri_org)
+    for k in range(num_forg):
+        globaldesc_tri_forg.append((abs(hist_tri_org[i]-hist_tri_forg[k]),0))
+    globaldesc_tri_temp.append(globaldesc_tri_forg)
+for i in range(num_org):
+    globaldesc_tri.append(globaldesc_tri_temp[count_tri]+globaldesc_tri_temp[count_tri+1])
+    count_tri += 2
     
 #globaldesc=[[(np.zeros(k),0)]*10]*5
 ##globaldesc = np.zeros((5,10))
